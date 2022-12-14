3f3f253111ab184eef64a6bcff77f0cc2e2787bb
Baako Alhassan Mohammed <baakomohammed95@gmail.com>
2022-12-14 11:13:26 +0000

    Create routes for homepage and shop page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import './App.css';
+
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<HomePage/> } />
+        <Route path='/shop' element={<ShopPage/>} />
+      </Routes>
+    </Router>
+    
+    
   );
 }
 
