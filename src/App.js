ae045688ad859abc8b2bcf7647153f223d398209
Baako Alhassan Mohammed <baakomohammed95@gmail.com>
2022-12-16 10:46:11 +0000

    Create site header with links to Shop, Contact

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import './App.css';
 
+import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path='/' element={<HomePage/> } />
         <Route path='/shop' element={<ShopPage/>} />
