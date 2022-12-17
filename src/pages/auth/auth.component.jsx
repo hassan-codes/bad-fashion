d726c5cbd57e9dc8d30dd77676e697e77130e6a8
Baako Alhassan Mohammed <baakomohammed95@gmail.com>
2022-12-17 00:17:17 +0000

    Create auth page for login and register

---
+++ src/pages/auth/auth.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import './auth.styles.scss';
+
+import SignIn from "../../components/sign-in/sign-in.component";
+
+const AuthPage = () => (
+  <SignIn />
+);
+
+export default AuthPage;
