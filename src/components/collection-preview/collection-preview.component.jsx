3de8f3ee96ff643c89a741fb0d9417cbbe29bed0
Baako Alhassan Mohammed <baakomohammed95@gmail.com>
2023-01-04 22:28:50 +0000

    Display add-to-cart button on hover

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,19 +1,18 @@
 import React from "react";
-import './collection-preview.styles.scss';
 
 import CollectionItem from "../collection-item/collection-item.component";
 
-const CollectionPreview = ({title, items}) => (
+import "./collection-preview.styles.scss";
+
+const CollectionPreview = ({ title, items }) => (
   <div className="collection-preview">
     <div className="title">{title.toUpperCase()}</div>
     <div className="preview">
-      {
-        items
-        .filter((item, idx) => (idx < 4))
-        .map(({ id, ...otherItemProps }) => (
-          <CollectionItem key={id} {...otherItemProps}/>
-        ))
-      }
+      {items
+        .filter((item, idx) => idx < 4)
+        .map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
     </div>
   </div>
 );
