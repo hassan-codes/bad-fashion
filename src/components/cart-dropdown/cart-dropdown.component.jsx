f82e71932fcc3a9275a924166ba0c7702b94abc0
Baako Alhassan Mohammed <baakomohammed95@gmail.com>
2023-01-09 16:45:12 +0000

    Display empty message in cart dropdown

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -11,16 +11,20 @@ import { createStructuredSelector } from "reselect";
 const CartDropdown = ({ cartItems }) => (
   <div className="cart-dropdown">
     <div className="cart-items">
-      {cartItems.map((cartItem) => (
-        <CartItem key={cartItem.id} item={cartItem} />
-      ))}
+      {cartItems.length ? (
+        cartItems.map((cartItem) => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))
+      ) : (
+        <span className="empty-message">Your cart is empty</span>
+      )}
     </div>
     <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>
 );
 
 const mapStateToProps = createStructuredSelector({
-  cartItems: selectCartItems(state),
+  cartItems: selectCartItems,
 });
 
 export default connect(mapStateToProps)(CartDropdown);
