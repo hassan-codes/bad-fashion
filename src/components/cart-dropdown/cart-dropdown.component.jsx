5cbd3da497d21239b69ae66e64f15da06ff6d365
Baako Alhassan Mohammed <baakomohammed95@gmail.com>
2023-01-11 23:09:48 +0000

    Hide cart dropdown when user navigates to checkout page

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -5,11 +5,12 @@ import { useNavigate } from "react-router-dom";
 import CustomButton from "../custom-button/custom-button.component";
 import CartItem from "../cart-item/cart-item.component";
 import { selectCartItems } from "../../redux/cart/cart.selectors";
+import { toggleCartHidden } from "../../redux/cart/cart.actions";
 
 import "./cart-dropdown.styles.scss";
 import { createStructuredSelector } from "reselect";
 
-const CartDropdown = ({ cartItems }) => {
+const CartDropdown = ({ cartItems, dispatch }) => {
   let navigate = useNavigate();
   return (
     <div className="cart-dropdown">
@@ -22,7 +23,12 @@ const CartDropdown = ({ cartItems }) => {
           <span className="empty-message">Your cart is empty</span>
         )}
       </div>
-      <CustomButton onClick={() => navigate("/checkout")}>
+      <CustomButton
+        onClick={() => {
+          navigate("/checkout");
+          dispatch(toggleCartHidden);
+        }}
+      >
         GO TO CHECKOUT
       </CustomButton>
     </div>
