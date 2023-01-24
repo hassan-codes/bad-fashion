68683cda4461a54c4d28bede98796a8b24c389a5
Baako Alhassan Mohammed <baakomohammed95@gmail.com>
2023-01-24 10:03:38 +0000

    Display item quantities in cart dropdown

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -7,7 +7,9 @@ const CartItem = ({ item: { imageUrl, name, price, quantity } }) => (
     <img src={imageUrl} alt={name} />
     <div className="item-details">
       <span className="name">{name}</span>
-      <span className="price">${price}</span>
+      <span className="price">
+        {quantity} x ${price}
+      </span>
     </div>
   </div>
 );
