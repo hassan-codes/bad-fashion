a1c9dd28c626004dda50f830b4418628d4ce927f
Baako Alhassan Mohammed <baakomohammed95@gmail.com>
2023-01-25 11:59:37 +0000

    Refactor code

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,55 +1,20 @@
 import React from "react";
-import "./directory.styles.scss";
+
 import MenuItem from "../menu-item/menu-item.component";
+import directorySectionsData from "./directory-sections.data";
+
+import "./directory.styles.scss";
 
 class Directory extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      sections: [
-        {
-          title: 'hats',
-          imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-          id: 1
-        },
-        {
-          title: 'jackets',
-          imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-          id: 2
-        },
-        {
-          title: 'sneakers',
-          imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-          id: 3
-        },
-        {
-          title: 'women\'s',
-          imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-          size: 'large',
-          id: 4
-        },
-        {
-          title: 'men\'s',
-          imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-          size: 'large',
-          id: 5
-        },
-      ],
-    };
-  };
-
-  render () {
+  render() {
     return (
       <div className="directory-menu">
-        {
-          this.state.sections.map(({ id, ...otherSectionProps}) => (
-            <MenuItem key={id} {...otherSectionProps} />
-          ))
-        }
+        {directorySectionsData.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
+        ))}
       </div>
     );
-  };
+  }
 }
 
 export default Directory;
